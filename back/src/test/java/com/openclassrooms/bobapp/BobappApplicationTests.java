fb97433c7a9eef2e73826dea422b69cd2a818eea
TangiLC <tangi@le-cadre.net>
2025-02-20 15:58:47 +0100

    sonar

--- back/src/test/java/com/openclassrooms/bobapp/BobappApplicationTests.java
+++ back/src/test/java/com/openclassrooms/bobapp/BobappApplicationTests.java
@@ -1,16 +1,25 @@
 package com.openclassrooms.bobapp;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class BobappApplicationTests {
 
+  @Autowired
+  private ApplicationContext context;
+
   @Test
-  void contextLoads() {}
+  void contextLoads() {
+    assertNotNull(context, "App context should not be null");
+  }
 
   @Test
   void testMain() {
     BobappApplication.main(new String[] {});
+    assertNotNull(context, "App context should not be null after start");
   }
 }
